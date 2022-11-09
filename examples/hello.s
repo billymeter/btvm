525ebed11c9cea678c838a4de06e7781cc6e06c1
Brad Trotter <bradley.trotter.efx@gmail.com>
2022-11-09 16:31:56 -0500

    updated hello.s to show that literals can be in hex or decimal.

--- examples/hello.s
+++ examples/hello.s
@@ -6,5 +6,6 @@ loop:
     output r1               ;; output character
     add r0, 1               ;; counter++
     compare r0, 0xd         ;; check if counter == 13
+    ; compare r0, 13        ;; alternate form of line 8
     jumpnoteq loop          ;; loop if counter != 13
 halt                        ;; stop the system
