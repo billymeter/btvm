a601021e53880869bebe1d8be3809b660bfceb62
Brad Trotter <billymeter@gmail.com>
2023-11-04 19:19:24 -0400

    added the ability to do naive imports in source code files in the assembler. the contents of an included file are swapped in place for the #include statement in the original source code file. this may cause issues for includes that have instructions in them, and should probably be included at the end of the file or include a jump instruction to jump over the included content into the main source file. recursive imports are currently not supported.

--- btasm.py
+++ btasm.py
@@ -4,15 +4,46 @@ the btvm assembler
 """
 
 from assembler.lexer import lex
-from assembler.parser import parse, symbol_table
+from assembler.parser import parse
 from assembler.assembler import assemble
-from common.common import *
+
+import os
+import shlex
 import sys
+
+
+def flatten(input_list):
+    flattened_list = []
+    for item in input_list:
+        if isinstance(item, list):
+            flattened_list.extend(flatten(item))
+        else:
+            flattened_list.append(item)
+    return flattened_list
+
+
+def resolve_imports(source, path="./"):
+    resolved = source
+    replacements = []
+    for i, line in enumerate(source):
+        l = shlex.split(line)
+
+        if l and l[0] == "#include":
+            with open(path + "/" + l[1]) as f:
+                lines = f.readlines()
+            replacements.append((i, lines))
+
+    for i, l in replacements:
+        del resolved[i]
+        resolved.insert(i, l)
+
+    return flatten(resolved)
 
 
 def main():
     with open(sys.argv[1]) as f:
         source = f.readlines()
+    source = resolve_imports(source, os.path.dirname(sys.argv[1]))
     lexed = lex(source)
     # print(lexed)
     parsed = parse(lexed)
